366b074fd541fb7904a1e7ae9a43a5c0fa8c3d1d
Satoshi Korin <sosuuuuu@gmail.com>
2019-07-15 15:35:51 +0900

    高校生割引1000円

--- src/main.rb
+++ src/main.rb
@@ -5,6 +5,7 @@ class Main
 
   def call
     return 1500 if @user_attr == :university_student
+    return 1000 if @user_attr == :high_school_student
     1900
   end
 end
--- src/main_test.rb
+++ src/main_test.rb
@@ -7,5 +7,8 @@ class TestMain < Minitest::Test
     assert_equal(1_500,
       Main.new(:university_student).(),
       'it should return \1500 if the user is university student')
+    assert_equal(1_000,
+      Main.new(:high_school_student).(),
+      'it should return \1000 if the user is high school student')
   end
 end
