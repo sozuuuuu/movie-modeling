41aa1c88bb6742eaab8537dda532c3a09485f26a
Satoshi Korin <sosuuuuu@gmail.com>
2019-07-15 15:34:42 +0900

    大学生割引1500円

--- src/main.rb
+++ src/main.rb
@@ -1,5 +1,10 @@
 class Main
+  def initialize(user_attr = nil)
+    @user_attr = user_attr
+  end
+
   def call
+    return 1500 if @user_attr == :university_student
     1900
   end
 end
--- src/main_test.rb
+++ src/main_test.rb
@@ -4,5 +4,8 @@ require './src/main'
 class TestMain < Minitest::Test
   def test_run
     assert_equal(1_900, Main.new.())
+    assert_equal(1_500,
+      Main.new(:university_student).(),
+      'it should return \1500 if the user is university student')
   end
 end
