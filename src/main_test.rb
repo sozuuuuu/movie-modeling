87169fdbc2410990183f3d524b22f34bb764a36a
Satoshi Korin <sosuuuuu@gmail.com>
2019-07-15 15:38:44 +0900

    grammar

--- src/main_test.rb
+++ src/main_test.rb
@@ -6,12 +6,12 @@ class TestMain < Minitest::Test
     assert_equal(1_900, Main.new.())
     assert_equal(1_500,
       Main.new(:university_student).(),
-      'it should return \1500 if the user is university student')
+      'it should return \1500 if the user is a university student')
     assert_equal(1_000,
       Main.new(:high_school_student).(),
-      'it should return \1000 if the user is high school student')
+      'it should return \1000 if the user is a high school student')
     assert_equal(1_000,
       Main.new(:child).(),
-      'it should return \1000 if the user is child')
+      'it should return \1000 if the user is a child')
   end
 end
