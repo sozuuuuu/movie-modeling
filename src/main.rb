1bbba5953f35af7089f1bab9eb97f4bdf279075c
Satoshi Korin <sosuuuuu@gmail.com>
2019-07-15 16:30:35 +0900

    障害者付き添い割引

--- src/main.rb
+++ src/main.rb
@@ -1,13 +1,30 @@
 class Main
   PRICE_1000_ATTRS = [:high_school_student, :child, :handicapped]
 
-  def initialize(user_attr = :none)
-    @user_attr = user_attr
+  def initialize(user_attrs = [:none])
+    @user_attrs = user_attrs.is_a?(Array) ? user_attrs : [user_attrs]
   end
 
   def call
-    return 1500 if @user_attr == :university_student
-    return 1000 if PRICE_1000_ATTRS.include?(@user_attr)
-    1900
+    # No instance variable definition outside of constructor!
+    @result = @user_attrs.map do |attr|
+      next 1500 if attr == :university_student
+      next 1000 if PRICE_1000_ATTRS.include?(attr)
+      1900
+    end
+    return handicapped_attendant_discount if @user_attrs.include?(:handicapped)
+    @result
+  end
+
+  def handicapped_attendant_discount
+    # Bad code: stateful map
+    discount_count = 1
+    @result.map do |r|
+      if r > 1000 && discount_count > 0
+        discount_count = discount_count - 1
+        next 1000
+      end
+      r
+    end
   end
 end
--- src/main_test.rb
+++ src/main_test.rb
@@ -3,18 +3,24 @@ require './src/main'
 
 class TestMain < Minitest::Test
   def test_run
-    assert_equal(1_900, Main.new.())
-    assert_equal(1_500,
+    assert_equal([1_900], Main.new.())
+    assert_equal([1_500],
       Main.new(:university_student).(),
       'it should return \1500 if the user is a university student')
-    assert_equal(1_000,
+    assert_equal([1_000],
       Main.new(:high_school_student).(),
       'it should return \1000 if the user is a high school student')
-    assert_equal(1_000,
+    assert_equal([1_000],
       Main.new(:child).(),
       'it should return \1000 if the user is a child')
-    assert_equal(1_000,
+    assert_equal([1_000],
       Main.new(:handicapped).(),
       'it should return \1000 if the uset is a handicapped')
+    assert_equal([1_000, 1_000],
+      Main.new([:handicapped, :none]).call,
+      'it should return [\1000, \1000] if one of the users is a handicapped with oen attendant')
+    assert_equal([1_000, 1_000, 1_900],
+      Main.new([:handicapped, :none, :none]).call,
+      'it should return [\1000, \1000, \1900] if one of the users is a handicapped with more than one attendant')
   end
 end
