b80e1deb705721ccb238f3b61ab50ca950208769
Satoshi Korin <sosuuuuu@gmail.com>
2019-07-17 00:51:16 +0900

    複数属性与えられるように拡張

--- src/main.rb
+++ src/main.rb
@@ -1,28 +1,76 @@
+# typed: true
 require 'date'
 require 'holidays'
 require 'holidays/core_extensions/date'
+require 'sorbet-runtime'
 
 class Date
   include Holidays::CoreExtensions::Date
 end
 
+class ArrayHelper
+  def self.wrap(object)
+    if object.nil?
+      []
+    elsif object.respond_to?(:to_ary)
+      object.to_ary
+    else
+      [object]
+    end
+  end
+end
+
+class User < T::Struct
+  prop :sex, T.nilable(Symbol)
+  prop :handicapped, T::Boolean, default: false
+  prop :student_type, T.nilable(Symbol), default: nil
+  prop :is_child, T::Boolean, default: false
+
+  def self.build(attr)
+    return self.build_with_single_symbol(attr) if attr.is_a?(Symbol)
+    new(attr)
+  end
+
+  def self.build_with_single_symbol(attr)
+    u = User.new
+    u.handicapped = true if attr == :handicapped
+    u.student_type = attr if [:university_student, :high_school_student].include?(attr)
+    u.is_child = true if attr == :child
+    u
+  end
+
+  def child?
+    is_child
+  end
+
+  def handicapped?
+    handicapped
+  end
+end
+
+class Users < Array
+  def have_handicapped?
+    self.any? { |u| u.handicapped? }
+  end
+end
+
 class Main
   PRICE_1000_ATTRS = [:high_school_student, :child, :handicapped]
 
-  def initialize(user_attrs = [:none], movie_type = :standard, movie_starts_at = nil)
-    @user_attrs = user_attrs.is_a?(Array) ? user_attrs : [user_attrs]
+  def initialize(user_attrs_or_user = [:none], movie_type = :standard, movie_starts_at = nil)
+    @users = Users.new(ArrayHelper.wrap(user_attrs_or_user).map { |v| v.is_a?(User) ? v : User.build(v) })
     @movie_type = movie_type
     @movie_starts_at = movie_starts_at ? DateTime.parse(movie_starts_at) : nil
   end
 
   def standard_cost
     # No instance variable definition outside of constructor!
-    @result = @user_attrs.map do |attr|
-      next 1500 if attr == :university_student
-      next 1000 if PRICE_1000_ATTRS.include?(attr)
+    @result = @users.map do |u|
+      next 1500 if u.student_type == :university_student
+      next 1000 if u.student_type == :high_school_student || u.child? || u.handicapped?
       1900
     end
-    @result = handicapped_attendant_discount if @user_attrs.include?(:handicapped)
+    @result = handicapped_attendant_discount if @users.have_handicapped?
     @result = evening_discount if @movie_starts_at && (!@movie_starts_at.to_date.holiday?(:jp) && ('17:30'..'19:55').include?(@movie_starts_at.strftime('%H:%M')))
     @result
   end
--- src/main_test.rb
+++ src/main_test.rb
@@ -1,3 +1,4 @@
+# typed: true
 require 'minitest/autorun'
 require './src/main'
 
